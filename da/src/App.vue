5f7ce6b1359e4da888655bb67583ff26b412016c
Io666777 <dimaskitihin@gmail.com>
2025-05-08 20:33:39 +0300

    начинаем

---
+++ da/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<header class="flex">
+  <div>
+    <h2 class="text-lg font-bold">Лавка</h2>
+  </div>
+
+  <div class="flex justifu">
+    <ul class="flex space-x-4">
+      <li>Кольца</li>
+      <li>Браслеты</li>
+      <li>Серьги</li>
+    </ul>
+  </div>
+
+  <div>
+    <ul class="flex space-x-4">
+      <li>Корзина</li>
+      <li>Избранное</li>
+      <li>Профиль</li>
+    </ul>
+  </div>
+</header>
+
+</template>
