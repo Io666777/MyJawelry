3458740a58d5e0a91b876c1168a062a7e6ca5148
Io666777 <dimaskitihin@gmail.com>
2025-05-17 15:43:11 +0000

    кошачья темка

--- da/src/views/figma.vue
+++ da/src/views/figma.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center items-center min-h-screen fond-[inter]">
         <div class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg place-self-center">
-            <h1 class="font-semibold text-gray-800 text-center text-3xl ">Join our monthly newsletter</h1>
+            <p class="font-bold text-gray-800 text-center text-3xl ">Join our monthly newsletter</p>
 
             <input 
                 class="w-[300px] h-[50px] border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 place-self-center"
